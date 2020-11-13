ccfb5983873bbbf37814a00dcd5c6a267b9f805c
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2020-11-13 02:26:56 -0300

    Remove react-native-svg

--- android/app/src/main/java/com/simaps/MainApplication.java
+++ android/app/src/main/java/com/simaps/MainApplication.java
@@ -1,6 +1,5 @@
 package com.simaps;
 
-import com.horcrux.svg.SvgPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new SvgPackage());
           return packages;
         }
 
